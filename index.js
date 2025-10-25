901b8a6b2063a985a840e487bab867fdc3f2b77b
andamagodwin <andamagodwinezra@gmail.com>
2025-10-25 16:02:59 +0300

    refactor: update project structure and simplify backend response

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require('express')
-const path = require('path')
 
 const port = process.env.PORT || 5006
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+
 
 app.get('/', (req, res) => {
-  console.log(`Rendering 'pages/index' for route '/'`)
-  res.render('pages/index')
+  res.send('Rydr backend code!')
 })
 
 const server = app.listen(port, () => {
